004772ddd60aa7ca16b7e963bf2b9108ede26c7d
fishfu <fishfu@gmail.com>
2021-07-03 01:17:28 +0000

    transmission openvpn running with pia port forwarding

---
+++ plex/transmission-openvpn/add_default_trackers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "please provide a torrent number"
+    exit 1
+fi
+
+for t in $(cat trackers-http.txt )
+do
+    echo "Adding tracker: $t"
+    transmission-remote transmission.local:80 --torrent $1 --tracker-add "${t}"
+done
