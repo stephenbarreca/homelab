a0a55b3068b8d0e717295271816ca0ea472b2d79
stephen <fishfu@gmail.com>
2022-05-23 14:10:17 -0400

    crashplan-pro security context

---
+++ charts/monitoring/prometheus-community/kube-prometheus-stack/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# uninstall script to uninstall kube-prometheus-stack that covers what helm does not
+
+kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
+kubectl delete crd alertmanagers.monitoring.coreos.com
+kubectl delete crd podmonitors.monitoring.coreos.com
+kubectl delete crd probes.monitoring.coreos.com
+kubectl delete crd prometheuses.monitoring.coreos.com
+kubectl delete crd prometheusrules.monitoring.coreos.com
+kubectl delete crd servicemonitors.monitoring.coreos.com
+kubectl delete crd thanosrulers.monitoring.coreos.com
