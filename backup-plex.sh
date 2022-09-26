c13b5d423d9739a8bee932d4a5e6eee7c27ede33
stephen <fishfu@gmail.com>
2022-09-26 11:00:52 -0400

    plex full backup

---
+++ backup-plex.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# plex directory to be backed up
+plexdir="/mnt/nvme/plex/config/Library/Application Support/Plex Media Server"
+# exclude cache directory
+plexcache="${plexdir}/Cache"
+
+# directory to store the backup
+bdir="/mnt/plex/movies/plex-backup"
+# backup archive path
+bfile="${bdir}/full-backup.bz2"
+btmp="${bfile}.tmp"
+
+echo "backing up plex to ${bfile}"
+
+# clean existing temp backup
+if [ -f "${btmp}" ]; then
+  rm "${btmp}"
+fi
+
+# make backup and store at temp location
+tar --exclude="${plexcache}" -jcvf "$btmp" "$plexdir"
+
+echo "rotating existing backups"
+if [ -f "${bfile}.2" ]; then
+  rm "${bfile}.2"
+fi
+if [ -f "${bfile}.1" ]; then
+  mv "${bfile}.1" "${bfile}.2"
+fi
+if [ -f "${bfile}" ]; then
+  mv "${bfile}" "${bfile}.1"
+fi
+
+# rename temp backup
+mv "${btmp}" "${bfile}"
+
+echo "backup complete"
